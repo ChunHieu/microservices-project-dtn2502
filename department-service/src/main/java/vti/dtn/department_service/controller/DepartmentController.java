2c1db0c8ea0bdc000e93c632c7f91955d5cffb58
Chun Hieu <caoduchieu26102003@gmail.com>
2025-07-03 09:40:34 +0700

    Init project

---
+++ department-service/src/main/java/vti/dtn/department_service/controller/DepartmentController.java
@@ -0,0 +1,20 @@
+package vti.dtn.department_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import vti.dtn.department_service.dto.DepartmentDTO;
+import vti.dtn.department_service.services.DepartmentService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/v1/departments")
+public class DepartmentController {
+    private final DepartmentService departmentService;
+
+    public List<DepartmentDTO> getAllDepartment(){
+        return departmentService.getAllDepartments();
+    }
+}
