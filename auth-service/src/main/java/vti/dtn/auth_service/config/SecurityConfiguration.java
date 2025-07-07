985deb33a95419d3ebcca6f7cad13715c6eaa39d
Chun Hieu <caoduchieu26102003@gmail.com>
2025-07-07 08:48:49 +0700

    update

--- auth-service/src/main/java/vti/dtn/auth_service/config/SecurityConfiguration.java
+++ auth-service/src/main/java/vti/dtn/auth_service/config/SecurityConfiguration.java
@@ -1,22 +1,39 @@
 package vti.dtn.auth_service.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import vti.dtn.auth_service.entity.enums.Role;
+import vti.dtn.auth_service.oauth2.handler.OAuth2AuthenticationFailureHandler;
+import vti.dtn.auth_service.oauth2.handler.OAuth2AuthenticationSuccessHandler;
+import vti.dtn.auth_service.oauth2.repository.HttpCookieOAuthorizationRequestRepository;
+import vti.dtn.auth_service.oauth2.service.CustomOAuth2UserService;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfiguration {
     private static final String[] WHITE_LIST_URL = {
+            "/oauth2/redirect",
+            "/oauth2/authorize",
+            "/oauth2/authorize/github",
+            "/oauth2/callback/github",
+            "/oauth2/redirect",
             "/api/v1/auth/login",
             "/api/v1/auth/register",
-            "/api/v1/auth/refresh-token",
-            "/api/v1/auth/verify",
+            "/api/v1/auth/refresh-token"
     };
 
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final HttpCookieOAuthorizationRequestRepository httpCookieOAuthorizationRequestRepository;
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
+
+    @SuppressWarnings("removal")
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -24,13 +41,23 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(req ->
                         req.requestMatchers(WHITE_LIST_URL)
                                 .permitAll()
+                                .requestMatchers("/api/v1/accounts").hasAnyRole(Role.ADMIN.name(), Role.MANAGER.name())
                                 .anyRequest()
-                                .authenticated()
-                )
-                .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
-
+                                .authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2Login()
+                .authorizationEndpoint()
+                .baseUri("/oauth2/authorize")
+                .authorizationRequestRepository(httpCookieOAuthorizationRequestRepository)
+                .and()
+                .redirectionEndpoint()
+                .baseUri("/oauth2/callback/*")
+                .and()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService)
+                .and()
+                .successHandler(oAuth2AuthenticationSuccessHandler)
+                .failureHandler(oAuth2AuthenticationFailureHandler);
         return http.build();
     }
 }
